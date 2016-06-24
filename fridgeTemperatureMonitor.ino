bd1598c88025418b1147ab192a673431723e9ec5
ceajog <gustavo.gonnet@ericsson.com>
2016-06-24 00:24:44 -0400

    version 0.03e
     * changes in version 0.03:
          * refactoring variables into arrays to simplify code
          * renaming the app to TemperatureMonitor (it's a bit more generic)
          * if thresholds are set to 0 that means they are deactivated
          * added an alarm led in the blynk app

--- fridgeTemperatureMonitor.ino
+++ fridgeTemperatureMonitor.ino
@@ -1,63 +1,68 @@
-#include "math.h"
-#include "application.h"
-#include "elapsedMillis.h"
-#include "blynk.h"
+#include <math.h>
+#include <application.h>
 #include "blynkAuthToken.h"
 
-#define APP_NAME "FridgeTemperatureMonitor"
-const String VERSION = "Version 0.02";
+/*******************************************************************************
+ These INCLUDE statements work when building in https://build.particle.io/build
+ Their usage depends on where you are building this software
+*******************************************************************************/
+// #include "elapsedMillis/elapsedMillis.h"
+// #include "blynk/blynk.h"
+/*******************************************************************************
+ These INCLUDE statements work when building locally
+ Their usage depends on where you are building this software
+*******************************************************************************/
+#include "elapsedMillis.h"
+#include "blynk.h"
+
+#define APP_NAME "TemperatureMonitor"
+const String VERSION = "Version 0.03e";
 
 /*******************************************************************************
-  the sensor used: 10K Precision Epoxy Thermistor - 3950 NTC
-  https://www.adafruit.com/products/372
-*******************************************************************************/
-//resistance at 77 Fahrenheit or 25 degrees Celsius
-#define THERMISTORNOMINAL 10000
-//temp. for nominal resistance (almost always 25 C)
-#define TEMPERATURENOMINAL 25
-//how many samples to take and average, more samples take longer
-// but measurement is 'smoother'
-#define NUMSAMPLES 5
-//The beta coefficient of the thermistor (usually 3000-4000)
-#define BCOEFFICIENT 3950
-//the value of the other resistor in series
-#define SERIESRESISTOR 10000
-/******************************************************************************/
+ * changes in version 0.01:
+       * initial version, 4 sensors are being monitored
+       * blynk app: http://tinyurl.com/z8osw3z
+ * changes in version 0.02:
+       * added threshold alarm for sensor1
+ * changes in version 0.03:
+      * refactoring variables into arrays to simplify code
+      * renaming the app to TemperatureMonitor (it's a bit more generic)
+      * if thresholds are set to 0 that means they are deactivated
 
-/*******************************************************************************
- IO mapping
+TODO:
+  * set thresholds from the blynk app
+  * store thresholds for alarms in eeprom
+  * pushbullet notifications of alarms
 *******************************************************************************/
-// A0 : thermistor 1
-// A1 : thermistor 2
-// A2 : thermistor 3
-// A3 : thermistor 4
-int INPUT_SENSOR1 = A0;
-int INPUT_SENSOR2 = A1;
-int INPUT_SENSOR3 = A2;
-int INPUT_SENSOR4 = A3;
 
 /*******************************************************************************
  Sensors
 *******************************************************************************/
-//Strings to store the sensors' reading so it can be exposed in the Particle Cloud
-String sensor1;
-String sensor2;
-String sensor3;
-String sensor4;
-
-//we will read one after the other and this is an index that will help us keep track
-// of which one we need to read next
-// IMPORTANT: index starts at 1
-int sensorToRead = 1;
-
 //defines the maximum number of sensors in case one day we add more
+// sensor0, sensor1, sensor2...
 #define MAX_NUMBER_OF_SENSORS 4
+
+//the plan is to read one after the other and this is an index that will help to keep track
+// of which sensor we need to read next
+// IMPORTANT: index starts at 0
+int sensorToRead = 0;
 
 //defines how often the measurements are made (millisecs)
 #define SENSOR_SAMPLE_INTERVAL 5000
-
-//this is a library timer
 elapsedMillis sensorSampleInterval;
+
+//array of Strings to store the sensors' readings so it can be exposed in the Particle Cloud
+String sensorReading[MAX_NUMBER_OF_SENSORS];
+
+/*******************************************************************************
+ IO mapping
+*******************************************************************************/
+// A0 : thermistor 0
+// A1 : thermistor 1
+// A2 : thermistor 2
+// A3 : thermistor 3
+// and so on...
+const int INPUT_SENSOR[8] = { A0, A1, A2, A3, A4, A5, A6, A7 };
 
 /*******************************************************************************
  alarms variables for each sensor
@@ -85,10 +90,7 @@ unsigned long alarmSensor4_next_alarm = 0;
 /*******************************************************************************
  thresholds for each sensor
 *******************************************************************************/
-float thresholdSensor1 = 50;
-float thresholdSensor2 = 50;
-float thresholdSensor3 = 50;
-float thresholdSensor4 = 50;
+float sensorThreshold[MAX_NUMBER_OF_SENSORS] = { 0, 0, 0, 0 };
 
 /*******************************************************************************
  Other variables
@@ -105,6 +107,23 @@ const int TIME_ZONE = -4;
 bool useFahrenheit = true;
 
 /*******************************************************************************
+  sensor used: 10K Precision Epoxy Thermistor - 3950 NTC
+  https://www.adafruit.com/products/372
+*******************************************************************************/
+//resistance at 77 Fahrenheit or 25 degrees Celsius
+#define THERMISTORNOMINAL 10000
+//temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25
+//how many samples to take and average, more samples take longer
+// but measurement is 'smoother'
+#define NUMSAMPLES 5
+//The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+//the value of the other resistor in series
+#define SERIESRESISTOR 10000
+/******************************************************************************/
+
+/*******************************************************************************
  Here you decide if you want to use Blynk or not
  Your blynk token goes in another file to avoid sharing it by mistake
  The file containing your blynk auth token has to be named blynkAuthToken.h and it should contain
@@ -116,14 +135,29 @@ bool useFahrenheit = true;
 char auth[] = BLYNK_AUTH_TOKEN;
 
 //definitions for the blynk interface
+#define BLYNK_DISPLAY_SENSOR0 V0
 #define BLYNK_DISPLAY_SENSOR1 V1
 #define BLYNK_DISPLAY_SENSOR2 V2
 #define BLYNK_DISPLAY_SENSOR3 V3
-#define BLYNK_DISPLAY_SENSOR4 V4
+#define BLYNK_DISPLAY_THRESHOLD0 V10
 #define BLYNK_DISPLAY_THRESHOLD1 V11
 #define BLYNK_DISPLAY_THRESHOLD2 V12
 #define BLYNK_DISPLAY_THRESHOLD3 V13
-#define BLYNK_DISPLAY_THRESHOLD4 V14
+#define BLYNK_DISPLAY_SENSORS { V0, V1, V2, V3, V4, V5, V6, V7 }
+#define BLYNK_DISPLAY_THRESHOLDS { V10, V11, V12, V13, V14, V15, V16, V17 }
+#define BLYNK_LED_ALARM_SENSOR0 V20
+#define BLYNK_LED_ALARM_SENSOR1 V21
+#define BLYNK_LED_ALARM_SENSOR2 V22
+#define BLYNK_LED_ALARM_SENSOR3 V23
+
+WidgetLED blynkAlarmSensor0(BLYNK_LED_ALARM_SENSOR0); //register led to virtual pin V20
+WidgetLED blynkAlarmSensor1(BLYNK_LED_ALARM_SENSOR1); //register led to virtual pin V21
+WidgetLED blynkAlarmSensor2(BLYNK_LED_ALARM_SENSOR2); //register led to virtual pin V22
+WidgetLED blynkAlarmSensor3(BLYNK_LED_ALARM_SENSOR3); //register led to virtual pin V23
+
+//this defines how often the readings are sent to the blynk cloud (millisecs)
+#define BLYNK_STORE_INTERVAL 5000
+elapsedMillis blynkStoreInterval;
 
 /*******************************************************************************
  here we define the frequency of the alarms sent to the user
@@ -145,26 +179,26 @@ void setup() {
   //publish startup message with firmware version
   Particle.publish(APP_NAME, VERSION, 60, PRIVATE);
 
-  //declare and init pins
-  pinMode(INPUT_SENSOR1, INPUT);
-  pinMode(INPUT_SENSOR2, INPUT);
-  pinMode(INPUT_SENSOR3, INPUT);
-  pinMode(INPUT_SENSOR4, INPUT);
+  //declare and init all analog pins as inputs
+  uint8_t i;
+  for (i=0; i < 8; i++) {
+    pinMode(INPUT_SENSOR[i], INPUT);
+  }
 
   //declare cloud variables
   //https://docs.particle.io/reference/firmware/photon/#particle-variable-
   //Currently, up to 10 cloud variables may be defined and each variable name is limited to a maximum of 12 characters
-  if (Particle.variable("sensor1", sensor1)==false) {
+  if (Particle.variable("sensor0", sensorReading[0])==false) {
+    Particle.publish(APP_NAME, "ERROR: Failed to register variable sensor0", 60, PRIVATE);
+  }
+  if (Particle.variable("sensor1", sensorReading[1])==false) {
     Particle.publish(APP_NAME, "ERROR: Failed to register variable sensor1", 60, PRIVATE);
   }
-  if (Particle.variable("sensor2", sensor2)==false) {
+  if (Particle.variable("sensor2", sensorReading[2])==false) {
     Particle.publish(APP_NAME, "ERROR: Failed to register variable sensor2", 60, PRIVATE);
   }
-  if (Particle.variable("sensor3", sensor3)==false) {
+  if (Particle.variable("sensor3", sensorReading[3])==false) {
     Particle.publish(APP_NAME, "ERROR: Failed to register variable sensor3", 60, PRIVATE);
-  }
-  if (Particle.variable("sensor4", sensor4)==false) {
-    Particle.publish(APP_NAME, "ERROR: Failed to register variable sensor4", 60, PRIVATE);
   }
 
   //init Blynk
@@ -182,7 +216,11 @@ void setup() {
  *******************************************************************************/
 void loop() {
 
-  //is time to read a sensor? if so, do it
+  if (USE_BLYNK == "yes") {
+    Blynk.run();
+  }
+
+  //is it time to read a sensor? if so, do it
   if (sensorSampleInterval > SENSOR_SAMPLE_INTERVAL) {
 
     //reset timer
@@ -192,7 +230,7 @@ void loop() {
     float sensorReading = readSensor(sensorToRead);
 
     //publish and expose in the Particle Cloud and blynk server the reading of the sensor
-    publishSensorReading(sensorToRead, sensorReading);
+    publishsensorReading(sensorToRead, sensorReading);
 
     //verify the reading has not exceeded the threshold
     if ( thresholdExceeded(sensorToRead, sensorReading) ){
@@ -202,15 +240,34 @@ void loop() {
 
     //increment the sensor to read
     sensorToRead = sensorToRead + 1;
-    if ( sensorToRead > MAX_NUMBER_OF_SENSORS ) {
-      sensorToRead = 1;
+    if ( sensorToRead == MAX_NUMBER_OF_SENSORS ) {
+      sensorToRead = 0;
     }
+
+    if (USE_BLYNK == "yes") {
+      BLYNK_setAlarmLed0(alarmSensor1);
+    }
+
+    //debug
+    Particle.publish("debug", String(sensorThreshold[0]), 60, PRIVATE);
 
   }
 
-  if (USE_BLYNK == "yes") {
-    //all the Blynk magic happens here
-    Blynk.run();
+  //publish readings to the blynk server every minute so the History Graph gets updated
+  // even when the blynk app is not on
+  //is it time to store in the blynk cloud? if so, do it
+  if (blynkStoreInterval > BLYNK_STORE_INTERVAL) {
+
+    //reset timer
+    blynkStoreInterval = 0;
+
+    if (USE_BLYNK == "yes") {
+      Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR0, sensorReading[0]);
+      Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR1, sensorReading[1]);
+      Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR2, sensorReading[2]);
+      Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR3, sensorReading[3]);
+    }
+
   }
 
 }
@@ -265,12 +322,12 @@ float readSensor( int sensorIndex )
 }
 
 /*******************************************************************************
- * Function Name  : publishSensorReading
+ * Function Name  : publishsensorReading
  * Description    : the temperature passed as parameter gets stored in an internal variable
                     and then published to the Particle Cloud and the blynk server
  * Return         : 0
  *******************************************************************************/
-int publishSensorReading( int sensorIndex, float temperature ) {
+int publishsensorReading( int sensorIndex, float temperature ) {
 
   char currentTempChar[32];
   int currentTempDecimals = (temperature - (int)temperature) * 100;
@@ -285,44 +342,26 @@ int publishSensorReading( int sensorIndex, float temperature ) {
   sprintf(currentTempChar,"%0d.%d", (int)temperature, currentTempDecimals);
 
   //store readings into exposed variables in the Particle Cloud
-  switch (sensorIndex)
-  {
-    case 1:
-      sensor1 = String(currentTempChar);
-      tempToBePublished = tempToBePublished + sensor1;
-      break;
-    case 2:
-      sensor2 = String(currentTempChar);
-      tempToBePublished = tempToBePublished + sensor2;
-      break;
-    case 3:
-      sensor3 = String(currentTempChar);
-      tempToBePublished = tempToBePublished + sensor3;
-      break;
-    case 4:
-      sensor4 = String(currentTempChar);
-      tempToBePublished = tempToBePublished + sensor4;
-      break;
-  }
+  sensorReading[sensorIndex] = String(currentTempChar);
+  tempToBePublished = tempToBePublished + sensorReading[sensorIndex];
 
   //publish reading in the console logs of the dashboard at https://dashboard.particle.io/user/logs
   Particle.publish(APP_NAME, tempToBePublished + getTemperatureUnit(), 60, PRIVATE);
 
-  //publish reading to the blynk server so the History Graph gets updated even when the blynk app is not on
   if (USE_BLYNK == "yes") {
     switch (sensorIndex)
     {
       case 1:
-        BLYNK_READ(BLYNK_DISPLAY_SENSOR1);
+        BLYNK_READ(BLYNK_DISPLAY_SENSOR0);
         break;
       case 2:
-        BLYNK_READ(BLYNK_DISPLAY_SENSOR2);
+        BLYNK_READ(BLYNK_DISPLAY_SENSOR1);
         break;
       case 3:
-        BLYNK_READ(BLYNK_DISPLAY_SENSOR3);
+        BLYNK_READ(BLYNK_DISPLAY_SENSOR2);
         break;
       case 4:
-        BLYNK_READ(BLYNK_DISPLAY_SENSOR4);
+        BLYNK_READ(BLYNK_DISPLAY_SENSOR3);
         break;
     }
   }
@@ -338,22 +377,7 @@ int publishSensorReading( int sensorIndex, float temperature ) {
  *******************************************************************************/
 int readTheAnalogInput( int sensorIndex )
 {
-  int reading;
-
-  if (sensorIndex == 1) {
-    reading = analogRead(INPUT_SENSOR1);
-  }
-  if (sensorIndex == 2) {
-    reading = analogRead(INPUT_SENSOR2);
-  }
-  if (sensorIndex == 3) {
-    reading = analogRead(INPUT_SENSOR3);
-  }
-  if (sensorIndex == 4) {
-    reading = analogRead(INPUT_SENSOR4);
-  }
-
-  return reading;
+  return analogRead(INPUT_SENSOR[sensorIndex]);
 }
 
 /*******************************************************************************
@@ -378,27 +402,7 @@ String getTemperatureUnit() {
  *******************************************************************************/
 bool thresholdExceeded( int sensorIndex, float temperature ) {
 
-  //a temporary, local variable that will store the threshold for the selected sensor
-  float thresholdForSensor;
-
-  //store readings into exposed variables in the Particle Cloud
-  switch (sensorIndex)
-  {
-    case 1:
-      thresholdForSensor = thresholdSensor1;
-      break;
-    case 2:
-      thresholdForSensor = thresholdSensor2;
-      break;
-    case 3:
-      thresholdForSensor = thresholdSensor3;
-      break;
-    case 4:
-      thresholdForSensor = thresholdSensor4;
-      break;
-  }
-
-  if ( thresholdForSensor > temperature ) {
+  if ( sensorThreshold[sensorIndex] and ( temperature > sensorThreshold[sensorIndex] ) ) {
     return true;
   }
 
@@ -473,38 +477,67 @@ void sendAlarmToUser( int sensorIndex ) {
                     corresponding widget in the blynk app
                     source: http://docs.blynk.cc/#widgets-displays-value-display
  *******************************************************************************/
+BLYNK_READ(BLYNK_DISPLAY_SENSOR0) {
+  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR0, sensorReading[0]);
+}
 BLYNK_READ(BLYNK_DISPLAY_SENSOR1) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR1, sensor1);
+  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR1, sensorReading[1]);
 }
 BLYNK_READ(BLYNK_DISPLAY_SENSOR2) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR2, sensor2);
+  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR2, sensorReading[2]);
 }
 BLYNK_READ(BLYNK_DISPLAY_SENSOR3) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR3, sensor3);
+  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR3, sensorReading[3]);
 }
-BLYNK_READ(BLYNK_DISPLAY_SENSOR4) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_SENSOR4, sensor4);
+BLYNK_READ(BLYNK_DISPLAY_THRESHOLD0) {
+  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD0, sensorThreshold[0]);
 }
 BLYNK_READ(BLYNK_DISPLAY_THRESHOLD1) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD1, thresholdSensor1);
+  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD1, sensorThreshold[1]);
 }
 BLYNK_READ(BLYNK_DISPLAY_THRESHOLD2) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD2, thresholdSensor2);
+  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD2, sensorThreshold[2]);
 }
 BLYNK_READ(BLYNK_DISPLAY_THRESHOLD3) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD3, thresholdSensor3);
+  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD3, sensorThreshold[3]);
 }
-BLYNK_READ(BLYNK_DISPLAY_THRESHOLD4) {
-  Blynk.virtualWrite(BLYNK_DISPLAY_THRESHOLD4, thresholdSensor4);
+
+//this is a blynk slider
+// source: http://docs.blynk.cc/#widgets-controllers-slider
+BLYNK_WRITE(BLYNK_DISPLAY_THRESHOLD0) {
+   sensorThreshold[0] = float(param.asInt());
+}
+
+//this is a blynk led
+// source: http://docs.blynk.cc/#widgets-displays-led
+BLYNK_READ(BLYNK_LED_ALARM_SENSOR0) {
+  if ( alarmSensor1 ) {
+    blynkAlarmSensor0.on();
+  } else {
+    blynkAlarmSensor0.off();
+  }
+}
+
+void BLYNK_setAlarmLed0(bool alarm) {
+  if (USE_BLYNK == "yes") {
+    if ( alarm ) {
+      blynkAlarmSensor0.on();
+    } else {
+      blynkAlarmSensor0.off();
+    }
+  }
 }
 
 BLYNK_CONNECTED() {
+  Blynk.syncVirtual(BLYNK_DISPLAY_SENSOR0);
   Blynk.syncVirtual(BLYNK_DISPLAY_SENSOR1);
   Blynk.syncVirtual(BLYNK_DISPLAY_SENSOR2);
   Blynk.syncVirtual(BLYNK_DISPLAY_SENSOR3);
-  Blynk.syncVirtual(BLYNK_DISPLAY_SENSOR4);
+  Blynk.syncVirtual(BLYNK_DISPLAY_THRESHOLD0);
   Blynk.syncVirtual(BLYNK_DISPLAY_THRESHOLD1);
   Blynk.syncVirtual(BLYNK_DISPLAY_THRESHOLD2);
   Blynk.syncVirtual(BLYNK_DISPLAY_THRESHOLD3);
-  Blynk.syncVirtual(BLYNK_DISPLAY_THRESHOLD4);
+
+  BLYNK_setAlarmLed0(alarmSensor1);
+
 }
